eb9c8e2a5f245e9b3d6b0d49a3105314e888b869
qwertyuiop888 <albertsun0@gmail.com>
2016-06-29 18:30:08 -0500

    Add files via upload

---
+++ penguins.bat
@@ -0,0 +1,15 @@
+@echo off
+echo VIRUS DETECTED
+sleep 1
+echo SYSTEM FAILIURE SOON
+sleep 1
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+sleep 1
+echo SHUTDOWN IN 10
+echo start chrome https://www.youtube.com/watch?v=dQw4w9WgXcQ >"C:\Users\dtp\Documents\rr.bat"
